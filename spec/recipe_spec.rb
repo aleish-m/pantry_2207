783bac84cb0d329fdb8d6a84b43e0f3d59e4e6c1
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-02 09:51:59 -0600

    test: add test for recipe class

---
+++ spec/recipe_spec.rb
@@ -0,0 +1,45 @@
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+
+describe Recipe do
+
+  before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new
+  end
+
+  describe 'initialize' do
+
+    it "Recipe exists" do
+      expect(@recipe1).to be_an_instance_of(Recipe)
+
+    end
+
+  end
+
+  describe 'Recipe attributes' do
+    it "Recipe has a name" do
+      expect(@recipe1.name).to eq("Mac and Cheese")
+    end
+  end
+
+  describe 'Recipe ingredients' do
+
+    it "Recipe requires ingredients" do
+      expect(@recipe1.ingredients_required).to eq({})
+    end
+
+    it "Recipe adds ingredients and amount" do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      expect(@recipe1.ingredients_required).to eq({@ingredient1 => 6, @ingredient2 => 8})
+    end
+
+    it "Recipe lists the ingredients" do
+      expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+    end
+  end
+end
